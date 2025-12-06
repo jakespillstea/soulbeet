5543f2c36e1ece5f19a38d1c0efe95c28418053b
Terry Raimondo <terry@insynia.com>
2025-12-06 03:45:30 +0100

    Add favicon & title

--- web/src/main.rs
+++ web/src/main.rs
@@ -34,6 +34,7 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Title { "SoulBeet" }
 
         AuthProvider { Router::<Route> {} }
     }
